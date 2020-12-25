d1c00a1e4cc2f510e4405d06573217aa03c91b7e
aomine <aomine1745@gmail.com>
2020-12-25 14:04:30 -0500

    v1.0

--- src/components/ListOfgifs.js
+++ src/components/ListOfgifs.js
@@ -1,8 +1,10 @@
 import Gif from './Gif'
 
 export default function ListOfGifs ({ gifs }) {
+  if (!gifs.length) return <p>Loading...</p>
   return (
     <>
+    <h1>Gifs Perrones</h1>
       {gifs.map(({ id, title, url }) => (
         <Gif
           key={id}
