4a19eda2ec43520ac6ea9991a08413e79898ac61
aomine <aomine1745@gmail.com>
2021-01-10 07:54:07 -0500

    hot fix

--- src/components/layout/Hero/index.js
+++ src/components/layout/Hero/index.js
@@ -49,7 +49,7 @@ function Hero () {
               {RATINGS.map(e => <option key={e} value={e} className='Hero-select-option'>{e}</option>)}
             </select>
           </form>
-          <p>{times}</p>
+          <p>query change times: {times}</p>
         </div>
       </section>
       <div className="Hero-bottom-wave">
