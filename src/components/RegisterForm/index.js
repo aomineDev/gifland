ec3598cfe1d7b158eb68d04fbb9101746e061be0
aomine <aomine1745@gmail.com>
2021-01-20 14:06:24 -0500

    change formik hook for formik components

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,5 +1,4 @@
-import { useFormik } from 'formik'
-
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { signUp } from 'services/auth'
 
 import Button from 'components/shared/Button'
@@ -7,44 +6,69 @@ import Button from 'components/shared/Button'
 import './styles.css'
 
 export default function RegisterForm () {
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      password: ''
-    },
-    validate: values => {
-      const errors = {}
-      if (!values.username) errors.username = 'username is required.'
+  function validate (values) {
+    const errors = {}
+    if (!values.username) errors.username = 'username is required.'
 
-      if (!values.password) {
-        errors.password = 'password is required.'
-      } else if (values.password.length < 3) {
-        errors.password = 'Length must be greater than 3'
-      }
-
-      return errors
-    },
-    onSubmit: (values, { setFieldError }) => {
-      return signUp(values)
-        .catch(err => {
-          setFieldError('username', 'username is taken.')
-        })
+    if (!values.password) {
+      errors.password = 'password is required.'
+    } else if (values.password.length < 3) {
+      errors.password = 'Length must be greater than 3'
     }
-  })
+
+    return errors
+  }
+
+  function onSubmit (values, { setFieldError }) {
+    return signUp(values)
+    .catch(err => {
+      setFieldError('username', 'username is taken.')
+    })
+  }
 
   return (
-    <form onSubmit={formik.handleSubmit} className="c-register-form" autoComplete="off">
-      <h2 className="c-register-title">Register</h2>
-      <input type="text" name="username" onChange={formik.handleChange}  className="c-register-input" />
-      {formik.errors.username && <p className="c-register-error-message">{formik.errors.username}</p>}
-      <input type="password" name="password" onChange={formik.handleChange}  className="c-register-input" />
-      {formik.errors.password && <p className="c-register-error-message">{formik.errors.password}</p>}
-      <Button
-        type="submit"
-        isLoading={formik.isSubmitting}
-      >
-        Register
-      </Button>
-    </form>
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+      }}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      {formik => (
+        <Form className="c-register-form" autoComplete="off">
+          <h2 className="c-register-title">Register</h2>
+          <Field
+            type="text"
+            name="username"
+            className="c-register-input"
+            placeholder="Username"
+          />
+          <ErrorMessage
+            className="c-register-error-message"
+            name="username"
+            component="p"
+          />
+          <Field
+            type="password"
+            name="password"
+            className="c-register-input"
+            placeholder="Password"
+          />
+          <ErrorMessage
+            className="c-register-error-message"
+            name="password"
+            component="p"
+          />
+          <Button
+            type="submit"
+            isLoading={formik.isSubmitting}
+            isDisabled={!formik.isValid}
+          >
+            Register
+          </Button>
+        </Form>
+      )}
+    </Formik>
   )  
 }
